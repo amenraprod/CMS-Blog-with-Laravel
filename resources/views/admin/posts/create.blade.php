8d2618c719fe48513cf99736519b7d32d22be6e5
amenraprod <amenraprod@gmail.com>
2018-01-11 22:21:59 -0500

    added title and featured image to form

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -6,6 +6,22 @@
   <div class="panel-heading">
     Create a new post
   </div>
+<div class="panel-body">
+  <form action="/post/store" method="post">
+
+ {{  csrf_field() }}
+
+ <div class="form-group">
+
+   <label for="title">Title</label>
+
+    <input type="text" name="title" class="form-control">
+
+    <label for="featured">Featured Image</label>
+
+     <input type="file" name="featured" class="form-control">
+
+  </form>
 
 </div>
 @stop
