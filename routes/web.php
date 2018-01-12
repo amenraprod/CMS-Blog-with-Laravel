53349ffcb1d0d93bb3d9bd561dadca11016eb9bd
amenraprod <amenraprod@gmail.com>
2018-01-11 22:54:24 -0500

    adding missing close tag

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 
 
-Route::group(['prefix=> 'admin'],     function(){
+Route::group(['prefix'=> 'admin'], function(){
 
 
       Route::get('/post/create', [
